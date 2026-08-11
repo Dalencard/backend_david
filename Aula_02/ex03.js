d35699c486917483716d24ee583bac1ad7a9fd92
David <david.assuncao.ferreira@escola.pr.gov.br>
2026-08-10 23:04:38 -0300

    Delete Aula_02 directory

--- Aula_02/ex03.js
+++
@@ -1,9 +0,0 @@
-const input = require('prompt-sync')();
-
-const calcularDobro = (numero) => {
-    return numero * 2;}
-
-let valor=Number(input('Digite um número: '));
-let resultado=calcularDobro(valor);
-
-console.log(`O dobro do número é: ${resultado}`);
